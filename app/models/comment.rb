63c0f1ffe174c8c71f5a2d33793f67130c575e1f
leila-alderman <leila.alderman@gmail.com>
2019-03-07 16:44:58 -0500

    Add comment model validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :body, presence: true, length: { maximum: 600 }
+  validates :post_id, presence: true
+  validates :user_id, presence: true
 end
